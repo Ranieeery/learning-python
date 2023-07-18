5ad2b70d8f67a3784e06b7c9010b9bb04e6847eb
Ranieeery <raniery2003@hotmail.com>
2023-07-18 13:23:51 -0300

    fix: tuples

--- basic/tuples.py
+++ basic/tuples.py
@@ -33,8 +33,3 @@ print(carro1, carro2, carro3, carro4, carro5)
 
 # Comparação
 print(carros == ('Fusca', 'Gol', 'Palio', 'Uno', 'Celta'))
-print(carros < ('Fusca', 'Gol', 'Palio', 'Uno', 'Celta', 'Civic'))
-
-# Imutabilidade
-carros[0] = 'Ferrari' # TypeError: 'tuple' object does not support item assignment
-
